cb63b39fa723590736064e8bef8b81b884b6b8c4
suichanwa <augustinnegura746@gmail.com>
2026-02-17 21:18:46 +0200

    set up deps n build.gradle

--- src/main/java/flaxbeard/cyberware/common/integration/CyberwareMatterOverdriveCheck.java
+++ src/main/java/flaxbeard/cyberware/common/integration/CyberwareMatterOverdriveCheck.java
@@ -1,15 +1,15 @@
 package flaxbeard.cyberware.common.integration;
 
-import matteroverdrive.entity.android_player.AndroidPlayer;
-import matteroverdrive.entity.player.MOPlayerCapabilityProvider;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class CyberwareMatterOverdriveCheck {
-    public static boolean isPlayerAndroid(EntityPlayer entityPlayer){
-        AndroidPlayer androidPlayer = MOPlayerCapabilityProvider.GetAndroidCapability(entityPlayer);
-        if (androidPlayer != null && androidPlayer.isAndroid()){
-            return true;
-        }
+    
+    public static boolean isMatterOverdriveLoaded() {
         return false;
+    }
+    
+    // Add this method that was being called
+    public static boolean isPlayerAndroid(EntityPlayer player) {
+        return false; // Always return false since Matter Overdrive is not available
     }
 }
